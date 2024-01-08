fad6680dfa5410880762d83ac3797dcf419187e8
OydinJurayeva <oydinjorayeva1020@gmail.com>
2024-01-09 01:33:30 +0500

    new

--- src/Question.jsx
+++ src/Question.jsx
@@ -1,9 +1,9 @@
-// Question.js
+// Question.jsx
 import React from 'react';
 
 const Question = ({ question, answer }) => {
   return (
-    <div className="bg-white p-4 rounded-md shadow-md mb-4">
+    <div className="bg-white p-4 rounded-md shadow-md">
       <h3 className="text-lg font-semibold mb-2">{question}</h3>
       <p>{answer}</p>
     </div>
